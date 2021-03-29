5a60a7fcd283772080169b1272667cd74820a2fb
Samuel Seda <zyxsamys@gmail.com>
2021-03-28 20:57:51 -0600

    User controller      Update user      Delete user

--- user/src/main/java/com/sam/user/controller/UserController.java
+++ user/src/main/java/com/sam/user/controller/UserController.java
@@ -18,20 +18,38 @@ public class UserController {
 
     @GetMapping //Specify REST method GET
     public ResponseEntity<List <User>> findAll(){
+        //JUST ACTIVE USERS
         return ResponseEntity.ok(userService.findAll());
     }
 
     @PostMapping("/{id}") //Specify REST method POST
     public ResponseEntity<User> findById(@PathVariable("id") Integer id){
         return userService.findById(id)
-                .map(ResponseEntity::ok) //Response 200 status code plus object user
+                .map(ResponseEntity::ok) //Response 200 status code plus object user created
                 .orElseGet(()-> ResponseEntity.notFound().build()); //Response 404 status code
     }
-
 
     @PostMapping //Specify REST method POST
     public ResponseEntity<User> create(@RequestBody User user){
         user.setStatus("ACTIVE"); //Set status by default Active
         return new ResponseEntity<>(userService.create(user), HttpStatus.CREATED); //Return object with 201 status code
+    }
+
+    @PutMapping //Specify REST method PUT
+    public ResponseEntity<User> update(@RequestBody User user){
+        return userService.findById(user.getId())
+                .map(userFound -> ResponseEntity.ok(userService.update(user))) //Response 200 status code with object user updated
+                .orElseGet(()->ResponseEntity.notFound().build()); //Response 404 status code
+    }
+
+    @DeleteMapping("/{id}") //Specify REST method DELETE
+    public ResponseEntity<User> delete(@PathVariable("id") Integer id){
+        return userService.findById(id)
+                .map(userFound ->{
+                    //if (userFound.getStatus().equals("ACTIVE")) {
+                    userService.delete(id); //Delete user found
+                    return ResponseEntity.ok(userFound); //Response 200 status code with object user deleted
+                })
+                .orElseGet(()->ResponseEntity.notFound().build()); //Response 404 status code
     }
 }
