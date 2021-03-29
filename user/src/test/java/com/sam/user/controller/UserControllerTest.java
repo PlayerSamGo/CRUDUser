5eca14087ca969fc2d3d1a19b451d34546b82fca
PlayerSamGo <zyxsamys@gmail.com>
2021-03-29 16:17:45 -0600

    Unit test create, update, delete

--- user/src/test/java/com/sam/user/controller/UserControllerTest.java
+++ user/src/test/java/com/sam/user/controller/UserControllerTest.java
@@ -1,9 +1,13 @@
 package com.sam.user.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sam.user.model.User;
 import com.sam.user.service.UserService;
+import org.hamcrest.Matchers;
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -20,9 +24,9 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @RunWith(SpringRunner.class)
 @WebMvcTest
@@ -35,12 +39,12 @@ class UserControllerTest {
     UserService userService;
 
     @Test
-    void findByStatus() throws Exception {
+    void findByStatusUser200Status() throws Exception {
         when(userService.findByStatus("ACTIVE")).thenReturn(
                 Collections.emptyList()
         );
 
-        MvcResult mvcResult = mockMvc.perform(
+        mockMvc.perform(
                 MockMvcRequestBuilders.get("/api/user")
                 .accept(MediaType.APPLICATION_JSON))
                 .andDo(print())
@@ -52,33 +56,79 @@ class UserControllerTest {
     }
 
     @Test
-    void findById() throws Exception {
+    void findByIdUser200Status() throws Exception {
 
         User user = new User(1,"Sam","Seda",new Date(1990,03,17), "ACTIVE");
+        userService.create(user);
 
         when(userService.findById(1)).thenReturn(Optional.of(user));
 
-        MvcResult mvcResult = mockMvc.perform(
+        mockMvc.perform(
                 post("/api/user/1"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andReturn();
-
-        System.out.println(mvcResult.getResponse().getContentAsString());
 
         verify(userService).findById(1);
 
     }
 
     @Test
-    void create() {
+    void createUser201Status() throws Exception {
+
+        String json= "{" +
+                "\"firstName\": \"Samuel Ivan\", " +
+                "\"lastName\": \"Seda Legaria\", " +
+                "\"dateOfBirth\": \"1990-03-17T18:25:43.511Z\"" +
+                "}";
+
+        mockMvc.perform(
+                post("/api/user")
+                .contentType("application/json")
+                .content(json))
+                .andDo(print())
+                .andExpect(status().isCreated());
+
     }
 
     @Test
-    void update() {
+    void updateUser200Status() throws Exception {
+        User user = new User(1,"Sam","Seda",new Date(1990,03,17), "ACTIVE");
+        userService.create(user);
+
+        when(userService.findById(1)).thenReturn(Optional.of(user));
+
+        String json= "{" +
+                "\"id\": \"1\", " +
+                "\"firstName\": \"Samuel Ivan\", " +
+                "\"lastName\": \"Seda Legaria\", " +
+                "\"dateOfBirth\": \"1990-03-17T18:25:43.511Z\", " +
+                "\"status\": \"INACTIVE\"" +
+                "}";
+
+        mockMvc.perform(
+                put("/api/user")
+                .contentType("application/json")
+                .content(json))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        verify(userService).findById(1);
     }
 
     @Test
-    void delete() {
+    void deleteUser200Status() throws Exception {
+
+        User user = new User(1,"Sam","Seda",new Date(1990,03,17), "ACTIVE");
+        userService.create(user);
+
+        when(userService.findById(1)).thenReturn(Optional.of(user));
+
+        mockMvc.perform(
+                delete("/api/user/1"))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        verify(userService).findById(1);
     }
 }
